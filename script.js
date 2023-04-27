9fea65248af4f823fc7d3553de70856056355b87
natsm <natsmarinho@gmail.com>
2023-04-27 19:13:09 -0300

    perf: Codigo com 2 números depois da virgula e identifica quando não houver valor

--- script.js
+++ script.js
@@ -6,17 +6,22 @@ function showValue(){
     const valorDesconto = document.querySelector("#porcentagem-desconto").value;
     const dividindoValores = valorProduto / 100;
     const desconto = dividindoValores * valorDesconto;
-    console.log(desconto);
+    
 
     const valorElement = document.createElement("p");
+    valorElement.style.fontSize = "2em";
     
     const divDosResultados = document.querySelector(".valor-desconto");
     divDosResultados.appendChild(valorElement);
 
     if (desconto < 1){
-        valorElement.textContent = `O valor do desconto é de $${desconto} centavos`
+        valorElement.textContent = `O valor do desconto é de $${desconto.toFixed(2)} centavos.`
+    } else if (desconto == 1){
+        valorElement.textContent = `O valor do desconto é de $${desconto.toFixed(2)} real.`
+    } else if (valorDesconto == 0 || valorProduto == 0){
+        valorElement.textContent = "Insira um valor!"
     }
      else {
-        valorElement.textContent = `O valor do desconto é de $${desconto} reais`;
-    }
+        valorElement.textContent = `O valor do desconto é de $${desconto.toFixed(2)} reais.`;
+    } 
 }
